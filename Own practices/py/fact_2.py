d861ddcaab0cf5030b09663fcd8364cb245fe208
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-15 11:16:51 +0530

    Add files via upload

---
+++ Own practices/py/fact_2.py
@@ -0,0 +1,12 @@
+
+#using recursive function
+
+n = int(input("enter a num: "))
+
+def fact(n):
+    if n == 0 or n ==1:
+        return 1
+    
+    return n * fact(n-1)
+
+print("factorial of" , n ,"is " ,fact(n))
