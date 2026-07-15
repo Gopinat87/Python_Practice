01e0c06fe2f17df7d0c41e917572977695cfdb32
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-15 11:16:06 +0530

    Add files via upload

---
+++ Own practices/py/fibo_2.py
@@ -0,0 +1,13 @@
+
+# Print using Recursion function method
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+terms = int(input(" Enter the number of terms: "))
+
+print("Fibonacci Series:")
+for i in range(terms):
+    print(fibonacci(i), end=" ")
