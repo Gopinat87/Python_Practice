6a0d6cee362940f6013de22dd18b5d444574514a
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-07-15 11:13:20 +0530

    Add files via upload

---
+++ Own practices/java/fibo_1.java
@@ -0,0 +1,26 @@
+// print fibonocci series using loop
+import java.util.Scanner;
+
+public class fibo_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        sc.close();
+    }
+}
